01666e0cb1dc2c8fce8616b767f5e0eb814b88b2
mohammad <mgharebaghi82@gmail.com>
2023-10-31 16:52:29 +0330

    keypair generator

---
+++ src/keypair_generation.rs
@@ -0,0 +1,90 @@
+pub mod generation {
+    use std::{io::{stdout, Write}, fs::File};
+
+    use bip39::Mnemonic;
+    use crossterm::{
+        execute,
+        style::{Color, Print, ResetColor, SetForegroundColor, Stylize},
+    };
+    use seed15::random_seed;
+    use sp_core::{ecdsa, Pair};
+
+    pub fn keys_generate(answer: String, wallet: &mut String) {
+        if answer == "n".to_string() || answer == "N".to_string() {
+            let seed = random_seed();
+            let mnomenic = Mnemonic::from_entropy(&seed).unwrap();
+            let phrases = &mnomenic.to_string();
+            let keys = ecdsa::Pair::from_phrase(&phrases, None).unwrap();
+
+            execute!(
+                stdout(),
+                SetForegroundColor(Color::Green),
+                Print("Your private key:".bold()),
+                ResetColor
+            )
+            .unwrap();
+            println!("\n{}", phrases);
+
+            wallet.push_str(&keys.0.public().to_string());
+            let mut wallet_file = File::create("./wallet.dat").unwrap();
+            write!(wallet_file,"{}", wallet).unwrap();
+            
+            execute!(
+                stdout(),
+                SetForegroundColor(Color::Green),
+                Print("Your wallet address to receive rewards:".bold()),
+                ResetColor
+            )
+            .unwrap();
+            println!("\n{}", wallet);
+
+            execute!(
+                stdout(),
+                SetForegroundColor(Color::DarkYellow),
+                Print("Please keep Your private key and wallet address in a safe where and then enter Y to continue:\n".bold()),
+                ResetColor
+            )
+            .unwrap();
+
+            loop {
+                let mut save_answ = String::new();
+                std::io::stdin().read_line(&mut save_answ).unwrap();
+                if save_answ.trim() == "y" || save_answ.trim() == "Y" {
+                    break;
+                } else {
+                    execute!(
+                        stdout(),
+                        SetForegroundColor(Color::Red),
+                        Print(
+                            "If you save Your adresses please enter Y:\n"
+                                .bold()
+                        ),
+                        ResetColor
+                    )
+                    .unwrap();
+                }
+            }
+        } else {
+            if let Ok(keys) = ecdsa::Pair::from_phrase(&answer, None) {
+                wallet.push_str(&keys.0.public().to_string());
+                execute!(
+                    stdout(),
+                    SetForegroundColor(Color::Green),
+                    Print("Your wallet address:".bold()),
+                    ResetColor
+                )
+                .unwrap();
+                println!("\n{}", wallet);
+            } else {
+                execute!(
+                    stdout(),
+                    SetForegroundColor(Color::Red),
+                    Print("your phrases key is wrong, please enter a correct!\n".bold()),
+                    ResetColor
+                )
+                .unwrap();
+                wallet.push_str(&"emptey".to_string());
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use std::{fs, process::Command, io};
-
+use std::{fs, io, process::Command};
+mod keypair_generation;
+use keypair_generation::generation::keys_generate;
 fn main() {
+    let mut wallet = String::new();
+    let mut answer = String::new();
+    loop {
+        answer.clear();
+        wallet.clear();
+        println!("Enter phrases key or enter 'N' to generate one:");
+        std::io::stdin().read_line(&mut answer).unwrap();
+        keys_generate(answer.clone().trim().to_string(), &mut wallet);
+        if wallet != "emptey".to_string() {
+            break;
+        }
+    }
     let mut file = fs::File::create("/etc/systemd/system/relay-service.service").unwrap();
     let mut source = fs::File::open("relay-node.service").unwrap();
     io::copy(&mut source, &mut file).unwrap();
@@ -11,8 +24,16 @@ fn main() {
         .unwrap();
     println!("{}", command);
 
-    let start = Command::new("systemctl").arg("start").arg("relay-service").status().unwrap();
+    let start = Command::new("systemctl")
+        .arg("start")
+        .arg("relay-service")
+        .status()
+        .unwrap();
     println!("start status: {}", start);
-    let status = Command::new("systemctl").arg("status").arg("relay-service").status().unwrap();
+    let status = Command::new("systemctl")
+        .arg("status")
+        .arg("relay-service")
+        .status()
+        .unwrap();
     println!("status code: {}", status);
 }
