2b90f497f4da19f8553cf5c6cf9d1bbfed1db83d
mohammad <mgharebaghi82@gmail.com>
2023-10-31 17:46:27 +0330

    change service path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io, process::Command};
+use std::{fs::{self, File}, io,io::Write, process::Command, env};
 mod keypair_generation;
 use keypair_generation::generation::keys_generate;
 fn main() {
@@ -14,6 +14,13 @@ fn main() {
             break;
         }
     }
+
+    let mut path_of_cfg = File::create("/etc/relay_source.conf").unwrap();
+    let path = format!("PATH={}\\relay-service", env::current_dir().unwrap().display().to_string());
+    let mut path_file = File::create("./SERVICE_PATH.conf").unwrap();
+    write!(path_file, "{}", path).unwrap();
+    io::copy(&mut path_file, &mut path_of_cfg).unwrap();
+
     let mut file = fs::File::create("/etc/systemd/system/relay-service.service").unwrap();
     let mut source = fs::File::open("relay-node.service").unwrap();
     io::copy(&mut source, &mut file).unwrap();
